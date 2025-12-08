e050f5efdb7262fc16988fe86e69da8c817fc613
Yann Sze Wong <yswong@kth.se>
2025-12-08 14:05:43 +0100

    startup

--- src/main/java/org/example/startUp/Main.java
+++ src/main/java/org/example/startUp/Main.java
@@ -1,5 +1,17 @@
 package org.example.startUp;
 
-public class Main {
+import org.example.controller.Controller;
+import org.example.integration.courseLayoutDBException;
 
+public class Main {
+    /*
+    public static void main(String[] args) {
+        try {
+            new BlockingInterpreter(new Controller()).handleCmds();
+        } catch(courseLayoutDBException cldbe) {
+            System.out.println("Could not connect to Bank db.");
+            cldbe.printStackTrace();
+        }
+    }
+     */
 }
